0ef5015ec8e159789b4bcdbc7360c6f227564dbb
Stan Liu <stanleyliu@berkeley.edu>
2023-01-06 14:46:20 -0800

    Move compiled bundle to root

---
+++ _next/static/L-3yLaPTpxDzMmDp0KTAR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/ef6529d7-7ba21a8ea40f66a3.js","static/chunks/366-8215db2f5d6b3af6.js","static/chunks/pages/index-ec06ef754945708d.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
